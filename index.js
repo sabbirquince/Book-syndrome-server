548cee1ed35729e513ed7eb71e2412ea08218c07
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-03-31 04:27:04 +0600

    book add, book get done

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.PORT || 4022;
 require("dotenv").config();
 const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kyzsc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const ObjectId = require("mongodb").ObjectID;
 
 app.use(express.json());
@@ -10,6 +16,24 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+client.connect((err) => {
+  const collection = client.db("book-syndrome").collection("books");
+
+  app.post("/addBook", (req, res) => {
+    const bookInfo = req.body;
+    collection
+      .insertOne(bookInfo)
+      .then((result) => res.send(result.insertedCount > 0));
+  });
+
+  app.get("/books", (req, res) => {
+    collection.find({}).toArray((err, docs) => {
+      res.send(docs);
+      console.log(docs);
+    });
+  });
 });
 
 app.listen(port);
